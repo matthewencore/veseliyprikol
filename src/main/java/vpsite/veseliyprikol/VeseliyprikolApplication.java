54e87bc49313287feedfb5a340b292bcff17206a
matthewencore <kirillkalmykov88@gmail.com>
2025-01-14 14:09:51 +0300

    Первая версия, начало

---
+++ src/main/java/vpsite/veseliyprikol/VeseliyprikolApplication.java
@@ -0,0 +1,13 @@
+package vpsite.veseliyprikol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeseliyprikolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeseliyprikolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vpsite/veseliyprikol/VeseliyprikolApplicationTests.java
@@ -0,0 +1,13 @@
+package vpsite.veseliyprikol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeseliyprikolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
